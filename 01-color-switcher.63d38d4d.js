28cb11308f57664c34ab465bd793de8e62fc0b1e
vargasasha <57102496+vargasasha@users.noreply.github.com>
2023-04-23 13:26:57 +0000

    Deploying to gh-pages from @ vargasasha/goit-js-hw-09@20351ba2904617dd2fbdcbc0de7a4d75b5f2d7e7 🚀

---
+++ 01-color-switcher.63d38d4d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{t.disabled=!0,n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.63d38d4d.js.map
