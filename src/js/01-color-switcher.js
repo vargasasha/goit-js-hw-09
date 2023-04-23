20351ba2904617dd2fbdcbc0de7a4d75b5f2d7e7
Oleksandr Varga <vargasasha@gmail.com>
2023-04-23 16:26:25 +0300

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  timerId = setInterval(() => {
+    startBtn.disabled = true;
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
