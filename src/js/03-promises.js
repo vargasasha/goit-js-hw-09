ef22bf50d167b066a10515ea68c85ef2bb7bdcbd
Oleksandr Varga <vargasasha@gmail.com>
2023-04-25 20:43:20 +0300

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const form = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+// const submitBtn = document.querySelector('button');
+
+form.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+function onSubmit(evt) {
+  
+  evt.preventDefault();
+
+  let firstDelay = Number(delay.value);
+  let delayStep = Number(step.value);
+
+  for (let i = 0; i < amount.value; i += 1) {
+    createPromise(i + 1, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  form.reset()
 }
